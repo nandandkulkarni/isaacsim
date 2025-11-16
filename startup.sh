940e54de8fe167a194f8aaa822412f587a2200ef
nandandkulkarni <84347353+nandandkulkarni@users.noreply.github.com>
2025-11-16 02:22:05 -0500

    Update startup script to use isaacsim paths

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-bash /workspace/startup-part1-install.sh
-bash /workspace/startup-part2-install-vscode.sh
-bash /workspace/startup-part3-code-tunnel.sh
-bash /workspace/startup-part4-ssh.sh
-bash /workspace/startup-part5-xvfb.sh
-bash /workspace/startup-part6-vnc.sh
-bash /workspace/startup-part7-web.sh
-bash /workspace/startup-part8-vscode.sh
-bash /workspace/startup-part9-python.sh
+bash /workspace/isaacsim/startup-part1-install.sh
+bash /workspace/isaacsim/startup-part2-install-vscode.sh
+bash /workspace/isaacsim/startup-part3-code-tunnel.sh
+bash /workspace/isaacsim/startup-part4-ssh.sh
+bash /workspace/isaacsim/startup-part5-xvfb.sh
+bash /workspace/isaacsim/startup-part6-vnc.sh
+bash /workspace/isaacsim/startup-part7-web.sh
+bash /workspace/isaacsim/startup-part8-vscode.sh
+bash /workspace/isaacsim/startup-part9-python.sh
